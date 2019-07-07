8ca347ba9fa090c0125921a81de5630df13cc6f5
Pierluigi Di Lorenzo <pierluigi@sciabarra.com>
2019-07-07 15:02:06 +0200

    Issue #2 closed

--- kubepass/kubepass.py
+++ kubepass/kubepass.py
@@ -8,20 +8,23 @@ import sys
 
 parser = argparse.ArgumentParser(description='Create you kube cluster')
 parser.add_argument("size",
-    help="<size of the cluster>",
-    choices=['small', 'large', 'huge', 'destroy'])
+                    help="<size of the cluster>",
+                    choices=['small', 'large', 'huge', 'destroy'])
 parser.add_argument("count",
-    nargs="?",
-    type=int,
-    help="<worker-count>",
-    default="3",
-    choices=range(1, 6))
+                    nargs="?",
+                    type=int,
+                    help="<worker-count>",
+                    default="3",
+                    choices=range(1, 6))
+parser.add_argument("-d",
+                    "--dashboard",
+                    help="<deploy Web UI Dashboard>",
+                    action="store_true")
 
 args = parser.parse_args()
 
 size = (args.size)
 count = (args.count)
-
 
 # controllo che sia installato multipass e aggiungo multipass.exe al $PATH
 
@@ -30,8 +33,8 @@ if os.name == 'nt':
     MULTIPASS = "multipass.exe"
     if os.path.isdir(WINMULTIPASS):
         os.environ["PATH"] += os.pathsep + WINMULTIPASS + os.sep + "bin"
-    
-    
+
+
 if not os.path.isfile(WINMULTIPASS+os.sep+"bin"+os.sep+MULTIPASS):
     print("Install multipass 0.7.0, please.")
     print("https://github.com/CanonicalLtd/multipass/releases/tag/v0.7.0")
@@ -55,6 +58,12 @@ def build(COUNT, ARGS_MASTER, ARGS_WORKERS):
               ARGS_WORKERS+" --cloud-init "+YAML)
     os.system(MULTIPASS+" exec kube-master -- cloud-init status --wait")
     os.system(MULTIPASS+" exec kube-master -- wait-ready "+str(COUNT+1))
+
+    #nel caso venga specificata l'opzione --dashboard, viene deployata anche la web ui
+    if args.dashboard == True:
+        print("Install Web Ui Dashboard")
+        os.system("kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta1/aio/deploy/recommended.yaml")
+
     print("Ready!")
 
 
@@ -62,16 +71,16 @@ def destroy(COUNT):
     print("Deleting kube-master")
     os.system(MULTIPASS+" -v delete kube-master")
     for i in range(1, COUNT+1):
-        NODE="kube-node"+str(i)
+        NODE = "kube-node"+str(i)
         print("Deleting kube-worker"+str(i))
         os.system(MULTIPASS+" delete "+NODE)
     os.system(MULTIPASS+" -v purge")
 
 
 def are_you_sure():
-  answer = input("are you sure? (y/n) ")
-  if answer[0].lower() != 'y':
-    exit()
+    answer = input("are you sure? (y/n) ")
+    if answer[0].lower() != 'y':
+        exit()
 
 
 if size == 'small':
@@ -87,6 +96,6 @@ if size == 'huge':
     build(count, "-c 2 -d 25G -m 4G", "-c 2 -d 25G -m 4G")
 
 if size == 'destroy':
-   print("Destroying the cluster")
-   are_you_sure()
-   destroy(count)
+    print("Destroying the cluster")
+    are_you_sure()
+    destroy(count)
